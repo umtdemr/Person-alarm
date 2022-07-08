4ccb0bb6fa8bf71ba852f828429e4e12a3a51fe0
umtdemr <umitde692@gmail.com>
2022-07-08 17:06:42 +0300

    integrated telegrambot with image processing

---
+++ core/utils/tele_bot.py
@@ -0,0 +1,28 @@
+from nis import cat
+import telegram
+from django.conf import settings
+
+
+class TelegramBot():
+    _bot = telegram.Bot(settings.T_TOKEN)
+
+    def __init__(self) -> None:
+        self._bot = telegram.Bot(settings.T_TOKEN)
+    
+    def get_bot(self) -> telegram.Bot:
+        return self._bot
+
+    def get_me(self):
+        return self._bot.get_me()
+    
+    def send_photo(self, path: str,reply_message_id: int = None, caption: str = ""):
+        opened_file = open(path, 'rb')
+        message = self._bot.send_photo(
+            chat_id=settings.T_USER,
+            photo=opened_file,
+            caption=caption,
+            reply_to_message_id=reply_message_id
+        )
+        opened_file.close()
+        return message
+
