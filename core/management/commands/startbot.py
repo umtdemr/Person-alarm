088f41509bd80a7ce437f3ad0850270bfc04a352
umtdemr <umitde692@gmail.com>
2022-07-12 21:29:27 +0300

    added bot starter to commands

--- core/management/commands/startbot.py
+++ core/management/commands/startbot.py
@@ -1,8 +1,30 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+
+from telegram import (
+    Update
+)
+from telegram.ext import (
+    Updater, 
+    CommandHandler,
+    CallbackContext
+)
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('bot başladı')
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Starts telegram bot for getting sensors data'
 
     def handle(self, *args, **options):
-        self.stdout.write(self.style.SUCCESS('Bot starting'))
+        self.stdout.write(self.style.SUCCESS('Bot is starting...'))
+
+        updater = Updater(settings.T_TOKEN, use_context=True)
+        dp = updater.dispatcher
+
+        dp.add_handler(CommandHandler("start", start))
+
+        updater.start_polling()
+        updater.idle()
