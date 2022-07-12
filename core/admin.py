0e70e0ac9bb25932857abd8ba88f7f97c40e96bc
umtdemr <umitde692@gmail.com>
2022-07-12 20:22:58 +0300

    added singleton admin

--- core/admin.py
+++ core/admin.py
@@ -7,6 +7,19 @@ from core.models import (
 )
 
 
+class SingletonAdmin(admin.ModelAdmin):
+    actions = None
+
+    def has_add_permission(self, request):
+        permission = super().has_add_permission(request)
+        if permission and not self.model.objects.count():
+            return True
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 class SiteSettingsAdmin(admin.ModelAdmin):
     pass 
 
