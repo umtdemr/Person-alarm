8684b2ed7c93594a16b61b43bab0ca12cc134fe7
umtdemr <umitde692@gmail.com>
2022-07-12 21:23:09 +0300

    added manage command

---
+++ core/management/commands/startbot.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Bot starting'))
